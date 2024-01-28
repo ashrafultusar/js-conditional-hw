ac2c07d86a36ef70292f1b30ca309e47476c6532
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-01-28 21:37:35 +0600

    added task -1

---
+++ task-1.js
@@ -0,0 +1,19 @@
+// QUESTION 
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+// ANSWAR
+
+ const burgerPrice=400;
+
+if(burgerPrice>500){
+console.log('free Coke')
+}
+
+else{
+    console.log('coke 30tk')
+}
